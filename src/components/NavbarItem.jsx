5581afaba9f04f7fd9072a5c3370c4f6d8b6bf2a
YanaWhitte <shiro.kirio@gmail.com>
2021-07-03 15:36:47 +0300

    Responsive layout

--- src/components/NavbarItem.jsx
+++ src/components/NavbarItem.jsx
@@ -3,7 +3,7 @@ import s from './Sidebar.module.css';
 const NavbarItem = ({ classname, iconName, alt, linkName }) => {
   return <div className={classname}>
     <img src={process.env.PUBLIC_URL + `/img/icons/${iconName}.svg`} alt={alt} />
-    <a href="#">{linkName}</a>
+    <a href="#" className={s.link}>{linkName}</a>
   </div>
 }
 
